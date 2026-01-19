f745fc704e51aa5f84a9ecd48b656b060fdf76fd
leeeeeseojin <kkyuuu00@gmail.com>
2026-01-19 09:24:14 +0900

    design: 사용자 조회 결과 페이지 ui 구조 수정 및 반응형 디자인 적용

--- src/pages/userResultPage/entry/UserResultPage.jsx
+++ src/pages/userResultPage/entry/UserResultPage.jsx
@@ -1,9 +1,15 @@
+import GradientLayout from '../../../components/gradientLayout/GradientLayout'
+import GradientButton from '../../../components/gradientButton/GradientButton'
+import backIcon from '../../../assets/auth/auth-back-icon.svg'
 import './UserResultPage.css'
+import { useNavigate } from 'react-router-dom'
 
 // 사용자 이벤트 신청 조회 결과 페이지
 export default function UserResultPage() {
+  const navigate = useNavigate()
+
   // 더미 데이터
-  const resultType = 'error'
+  const resultType = 'det'
   const userInfo = {
     name: '조성찬',
     studentId: '0000000000',
@@ -20,35 +26,45 @@ export default function UserResultPage() {
     title2 = '해당 정보로 조회된 기록이 없습니다.'
   }
 
+  const handleGoBack = () => {
+    navigate(-1)
+  }
+
   return (
     // 전체 페이지
-    <div className='user-result'>
-      {/* 중앙 흰색 결과 카드 */}
-      <div className='user-result__card'>
-        <h1 className='user-result__title__01'>{userInfo.name}님,</h1>
-        <h2 className='user-result__title__02'>{title2}</h2>
+    <GradientLayout>
+      <div className='user-result__container'>
+        {/* 로고 영역 */}
+        <div className='user-result__logo'></div>
+        {/* 중앙 흰색 결과 카드 */}
+        <div className='user-result__card'>
+          <h1 className='user-result__title__01'>{userInfo.name}님</h1>
+          <h2 className='user-result__title__02'>{title2}</h2>
 
-        {/* 사용자 정보 */}
-        {resultType === 'detail' && (
-          <div className='user-result__box'>
-            <div className='user-result__info'>이름: {userInfo.name}</div>
-            <div className='user-result__info'>학번: {userInfo.studentId}</div>
-            <div className='user-result__info'>생년월일: {userInfo.birth}</div>
-            <div className='user-result__info'>제출 여부: {userInfo.submissionStatus}</div>
-          </div>
-        )}
+          {/* 사용자 정보 */}
+          {resultType === 'detail' && (
+            <div className='user-result__box'>
+              <span>이름:</span>
+              <span>{userInfo.name}</span>
 
-        <button
-          type='button'
-          className={
-            resultType === 'detail'
-              ? 'user-result__button__01'
-              : 'user-result__button__01 user-result__button__02'
-          }
-        >
-          돌아가기
-        </button>
+              <span>학번:</span>
+              <span>{userInfo.studentId}</span>
+
+              <span>생년월일:</span>
+              <span>{userInfo.birth}</span>
+
+              <span>제출 여부:</span>
+              <span>{userInfo.submissionStatus}</span>
+            </div>
+          )}
+        </div>
+        <div className='user-result__button'>
+          <GradientButton type='button' onClick={handleGoBack}>
+            <img src={backIcon} className='user-result__button-icon' />
+            <span>돌아가기</span>
+          </GradientButton>
+        </div>
       </div>
-    </div>
+    </GradientLayout>
   )
 }
