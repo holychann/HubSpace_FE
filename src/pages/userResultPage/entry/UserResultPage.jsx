d2e41061b0b6f59f997056425ffe5791b86e8f66
leeeeeseojin <kkyuuu00@gmail.com>
2026-01-20 07:52:33 +0900

    feat: 더미 api 호출 및 CSV/FORM 타입 따라서 결과 화면 분기

--- src/pages/userResultPage/entry/UserResultPage.jsx
+++ src/pages/userResultPage/entry/UserResultPage.jsx
@@ -2,32 +2,41 @@ import GradientLayout from '../../../components/gradientLayout/GradientLayout'
 import GradientButton from '../../../components/gradientButton/GradientButton'
 import backIcon from '../../../assets/auth/auth-back-icon.svg'
 import './UserResultPage.css'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { processUserResult } from '../utils/UserFieldConfig'
+
+import { getUserSearchResult, userEventConfig } from '../utils/UserResultDummy'
 
 // 사용자 이벤트 신청 조회 결과 페이지
 export default function UserResultPage() {
   const navigate = useNavigate()
+  const location = useLocation() // state로 넘긴 data 받음
 
-  // 더미 데이터
-  const resultType = 'det'
-  const userInfo = {
-    name: '조성찬',
-    studentId: '0000000000',
-    birth: '000000',
-    submissionStatus: '제출 완료',
+  const userSearchData = location.state?.userSearchData || {}
+
+  // 더미 api 호출
+  const userApiResponse = getUserSearchResult(userSearchData)
+
+  const userSearchResult = processUserResult(userApiResponse, userEventConfig, userSearchData)
+
+  // 타이틀 설정
+  let userName = ''
+  let userMessage = ''
+
+  if (userSearchResult.userResultType === 'notFound') {
+    userName = userSearchResult.userDisplayName
+    userMessage = userSearchResult.userResultMessage
+  } else if (userSearchResult.userResultType === 'success') {
+    userName = userSearchResult.userDisplayName
+    userMessage = '정보가 정상적으로 조회되었습니다.'
+  } else if (userSearchResult.userResultType === 'detail') {
+    userName = userSearchResult.userDetailInfo['이름']
+    userMessage = '정보가 아래와 같이 조회되었습니다.'
   }
 
-  let title2 = ''
-  if (resultType === 'success') {
-    title2 = '정보가 정상적으로 조회되었습니다.'
-  } else if (resultType === 'detail') {
-    title2 = '정보가 아래와 같이 조회되었습니다.'
-  } else {
-    title2 = '해당 정보로 조회된 기록이 없습니다.'
-  }
-
+  // 돌아가기 버튼 클릭 시 실행
   const handleGoBack = () => {
-    navigate(-1)
+    navigate(-1) // 브라우저 히스토리 기준 이전 페이지
   }
 
   return (
@@ -38,23 +47,23 @@ export default function UserResultPage() {
         <div className='user-result__logo'></div>
         {/* 중앙 흰색 결과 카드 */}
         <div className='user-result__card'>
-          <h1 className='user-result__title__01'>{userInfo.name}님</h1>
-          <h2 className='user-result__title__02'>{title2}</h2>
+          <h1 className='user-result__title__01'>{userName}님</h1>
+          <h2 className='user-result__title__02'>{userMessage}</h2>
 
           {/* 사용자 정보 */}
-          {resultType === 'detail' && (
+          {userSearchResult.userResultType === 'detail' && (
             <div className='user-result__box'>
-              <span>이름:</span>
-              <span>{userInfo.name}</span>
-
-              <span>학번:</span>
-              <span>{userInfo.studentId}</span>
-
-              <span>생년월일:</span>
-              <span>{userInfo.birth}</span>
+              {userSearchResult.userSearchColumns.map((columnName) => (
+                <>
+                  <span key={`label-${columnName}`}>{columnName}:</span>
+                  <span key={`value-${columnName}`}>
+                    {userSearchResult.userDetailInfo[columnName]}
+                  </span>
+                </>
+              ))}
 
               <span>제출 여부:</span>
-              <span>{userInfo.submissionStatus}</span>
+              <span>{userSearchResult.userDetailInfo['제출여부']}</span>
             </div>
           )}
         </div>
