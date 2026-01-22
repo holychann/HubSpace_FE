bee5d0bd0b2abe33eedd03411deb2ef0c60786b7
Eine0322 <eine113589@gmail.com>
2026-01-23 04:16:34 +0900

    fix : file name error

---
+++ src/pages/csvDetailPage/utils/CsvDetailDummy.js
@@ -0,0 +1,13 @@
+export const csvEvent = {
+  isSuccess: true,
+  data: {
+    id: '12462432',
+    count: 0,
+    eventTitle: '이러쿵저러쿵',
+    eventType: 'FILE',
+    searchColumns: ['컬럼1', '컬럼2', '컬럼3'],
+    displayColumn: '컬럼명',
+    isActive: true,
+    createdAt: '2024-11-07T12:36:22',
+  },
+}
